8f05f5fb2ad13093e1d3ac4357083f17c9e98875
Matt <matt.cosentino@learnedmedia.com>
2016-10-24 15:14:02 -0400

    products added

--- js/build/production.js
+++ js/build/production.js
@@ -1569,11 +1569,21 @@ responsive:!0,responsiveRefreshRate:200,responsiveBaseWidth:g,baseClass:"owl-car
 $(document).ready(function() {
 	$("#product-carousel").owlCarousel({
 		navigation : true,
-		navigationText : ["prev","next"],
+		navigationText : ['<i class="fa fa-angle-left fa-2x"></i>','<i class="fa fa-angle-right fa-2x"></i>'],
 		rewindNav : true,
 		scrollPerPage : false,
-		pagination : false
+		pagination : false,
+		mouseDrag : false
 	});
+	//shop now buttons
+	$(".owl-item").hover(
+	function() {
+		$(this).find("a").fadeIn('fast');
+	},
+	function() {
+		$(this).find("a").fadeOut('fast');
+	}
+	);
 });
 
 //smooth scroll all anchor links
@@ -1601,3 +1611,6 @@ var userFeed = new Instafeed({
 	template: '<a target="_blank" href="{{link}}" rel="ig" title="{{caption}}"><img class="instagram-image desktop-2 tablet-1 mobile-1 contained" src="{{image}}" /></a>'
 });
 userFeed.run();        
+
+
+
--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@
 $(document).ready(function() {
 	$("#product-carousel").owlCarousel({
 		navigation : true,
-		navigationText : ["prev","next"],
+		navigationText : ['<i class="fa fa-angle-left fa-2x"></i>','<i class="fa fa-angle-right fa-2x"></i>'],
 		rewindNav : true,
 		scrollPerPage : false,
-		pagination : false
+		pagination : false,
+		mouseDrag : false
 	});
+	//shop now buttons
+	$(".owl-item").hover(
+	function() {
+		$(this).find("a").fadeIn('fast');
+	},
+	function() {
+		$(this).find("a").fadeOut('fast');
+	}
+	);
 });
 
 //smooth scroll all anchor links
@@ -34,3 +44,6 @@ var userFeed = new Instafeed({
 	template: '<a target="_blank" href="{{link}}" rel="ig" title="{{caption}}"><img class="instagram-image desktop-2 tablet-1 mobile-1 contained" src="{{image}}" /></a>'
 });
 userFeed.run();        
+
+
+
